fd8a1a4d4b1df745756889defba969f890399219
LuisHsu <luishsuplus@gmail.com>
2022-07-30 02:22:01 -0700

    Setup webpack dev/prod environment

---
+++ README.md
@@ -0,0 +1,13 @@
+# MapleClient
+
+### Development run
+
+```shell
+npm start
+```
+
+### Build
+
+```shell
+npm run build
+```
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,41 @@
 const Path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = (env, argv) => ({
-    entry: './src/index.js',
-    output: {
-        filename: 'index.js',
-        path: Path.resolve(__dirname, 'dist'),
-    },
-    mode: env.DEBUG ? 'development' :  'production'
-});
+module.exports = (env, argv) => {
+    var config = {
+        entry: './src/index.js',
+        output: {
+            filename: 'index.js',
+            path: Path.resolve(__dirname, 'dist'),
+            clean: true,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'Maplestory Web Client',
+                template: './src/index.ejs'
+            }),
+        ]
+    };
+    if(env.DEBUG){
+        config.mode = 'development';
+        config.devtool = 'inline-source-map';
+        config.devServer = {
+            static: './dist',
+            client: {
+                progress: true,
+                reconnect: true,
+            },
+        };
+    }else{
+        config.mode = 'production';
+        config.optimization = {
+            minimize: true,
+            minimizer: [new TerserPlugin()],
+            splitChunks: {
+                chunks: 'all',
+            },
+        };
+    }
+    return config;
+};
