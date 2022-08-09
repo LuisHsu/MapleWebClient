17e96720b331b3ec2c99b6ef8f8b2271951e8226
LuisHsu <luishsuplus@gmail.com>
2022-08-09 03:19:01 -0700

    [WIP] Implement Cursor

--- src/graphics/Animation.ts
+++ src/graphics/Animation.ts
@@ -5,31 +5,37 @@
 
 import Setting from "../Setting";
 import {Texture} from "./Texture";
-import gl, { Drawable, Transform } from "./GL";
-import { Point, Size } from "../Types";
+import gl, {Drawable, Transform} from "./GL";
+import {Point, Size} from "../Types";
 
 export class Frame implements Drawable {
-    constructor(texture: Texture, delay: number, transform: Transform = new Transform, from?: Transform){
+    constructor(texture: Texture, delay?: number, transform: Transform = new Transform, from?: Transform){
         this.texture = texture;
-        this.delay = delay * 1000;
+        this.delay = delay ? (delay * 1000) : null;
         this.transform = transform;
         if(from){
             this.counter = 0;
             this.from = from;
         }
     }
-    draw(): void {
-        if(this.from && (this.counter <= this.delay)){
+    draw(transform: Transform): void {
+        if(this.delay && this.from && (this.counter <= this.delay)){
             gl.draw_texture(this.texture, new Transform({
-                rotate: this.from.rotate + ((this.transform.rotate - this.from.rotate) * this.counter / this.delay),
-                opacity: this.from.opacity + ((this.transform.opacity - this.from.opacity) * this.counter / this.delay),
+                rotate: this.from.rotate + transform.rotate +
+                    ((this.transform.rotate - this.from.rotate) * this.counter / this.delay),
+                opacity: this.from.opacity * transform.opacity *
+                    ((this.transform.opacity - this.from.opacity) * this.counter / this.delay),
                 scale: new Size(
-                    this.from.scale.width + ((this.transform.scale.width - this.from.scale.width) * this.counter / this.delay),
-                    this.from.scale.height + ((this.transform.scale.height - this.from.scale.height) * this.counter / this.delay)
+                    this.from.scale.width + transform.scale.width +
+                    ((this.transform.scale.width - this.from.scale.width) * this.counter / this.delay),
+                    this.from.scale.height + transform.scale.height +
+                    ((this.transform.scale.height - this.from.scale.height) * this.counter / this.delay)
                 ),
                 offset: new Point(
-                    this.from.offset.x + ((this.transform.offset.x - this.from.offset.x) * this.counter / this.delay),
-                    this.from.offset.y + ((this.transform.offset.y - this.from.offset.y) * this.counter / this.delay)
+                    this.from.offset.x + transform.offset.x +
+                    ((this.transform.offset.x - this.from.offset.x) * this.counter / this.delay),
+                    this.from.offset.y + transform.offset.y +
+                    ((this.transform.offset.y - this.from.offset.y) * this.counter / this.delay)
                 ),
             }));
             this.counter += Setting.FPS;
@@ -40,7 +46,7 @@ export class Frame implements Drawable {
     reset(): void{
         this.counter = 0;
     }
-    delay: number;
+    delay?: number;
     private texture: Texture;
     private transform: Transform;
     private counter?: number;
@@ -53,13 +59,13 @@ export class Animation implements Drawable{
         this.repeat = repeat;
     }
     start(){
-        if(this.timeout === null){
+        if((this.frames.length > 1) && (this.timeout === null)){
             this.frames[this.index].reset();
             this.timeout = setTimeout(this.update.bind(this), this.frames[this.index].delay);
         }
     }
     stop(){
-        if(this.timeout !== null){
+        if((this.frames.length > 1) && this.timeout !== null){
             clearTimeout(this.timeout);
             this.timeout = null;
         }
@@ -67,8 +73,8 @@ export class Animation implements Drawable{
     reset(){
         this.index = 0;
     }
-    draw(){
-        this.frames[this.index].draw();
+    draw(transform: Transform){
+        this.frames[this.index].draw(transform);
     }
     private repeat: boolean;
     private frames: Frame[];
--- src/io/Cursor.ts
+++ src/io/Cursor.ts
@@ -3,6 +3,33 @@
  * @module Cursor
  */
 
+import Animation, { Frame } from "../graphics/Animation";
+import { Drawable, Transform } from "../graphics/GL";
+import { Texture } from "../graphics/Texture";
+import { Point, Size } from "../Types";
+import Window from "./Window";
+
+export class Cursor implements Drawable {
+    public position: Point = new Point(Window.size.width / 2, Window.size.height / 2);
+    init(): void {
+        this.set_state(CursorState.Idle);
+    }
+    state(): CursorState {
+        return this._state;
+    }
+    set_state(state: CursorState): void {
+        this._animation = new Animation(CursorData[state].map(data => new Frame(
+            new Texture(`data/UI/Cursor/${data.path}`, data.offset, data.size),
+            data.delay
+        )), true);
+    }
+    draw(transform: Transform): void {
+
+    }
+    private _state: CursorState;
+    private _animation: Animation;
+};
+
 export enum CursorState {
     Idle = 0,
     CanClick,
@@ -19,76 +46,287 @@ export enum CursorState {
     RClick,
 }
 
-const CursorImage = [
+type CursorDataType = {
+    path: string,
+    size: Size,
+    offset: Point,
+    delay?: number,
+}
+
+const CursorData: CursorDataType[][] = [
     [
-        "Cursor/Cursor.0.0.png"
+        {
+            path: "Cursor.0.0.png",
+            size: new Size(24, 28),
+            offset: new Point(12, -25),
+        },
     ],
     [
-        "Cursor/Cursor.1.0.png",
-        "Cursor/Cursor.1.1.png",
+        {
+            path: "Cursor.1.0.png",
+            size: new Size(29, 30),
+            offset: new Point(11.5, -24),
+            delay: 500,
+        },
+        {
+            path: "Cursor.1.1.png",
+            size: new Size(30, 28),
+            offset: new Point(11, -24),
+            delay: 500,
+        },
     ],
     [
-        "Cursor/Cursor.1.0.png",
-        "Cursor/Cursor.1.1.png",
+        {
+            path: "Cursor.2.0.png",
+            size: new Size(32, 28),
+            offset: new Point(13, -22),
+            delay: 200,
+        },
+        {
+            path: "Cursor.2.1.png",
+            size: new Size(33, 26),
+            offset: new Point(12.5, -22),
+            delay: 100,
+        },
+        {
+            path: "Cursor.2.2.png",
+            size: new Size(33, 26),
+            offset: new Point(12.5, -22),
+            delay: 100,
+        },
+        {
+            path: "Cursor.2.3.png",
+            size: new Size(32, 28),
+            offset: new Point(13, -22),
+            delay: 100,
+        },
+        {
+            path: "Cursor.2.4.png",
+            size: new Size(32, 28),
+            offset: new Point(13, -22),
+            delay: 100,
+        },
+        {
+            path: "Cursor.2.5.png",
+            size: new Size(33, 26),
+            offset: new Point(12.5, -22),
+            delay: 100,
+        },
+        {
+            path: "Cursor.2.6.png",
+            size: new Size(33, 26),
+            offset: new Point(12.5, -22),
+            delay: 100,
+        },
     ],
     [
-        "Cursor/Cursor.2.0.png",
-        "Cursor/Cursor.2.1.png",
-        "Cursor/Cursor.2.2.png",
-        "Cursor/Cursor.2.3.png",
-        "Cursor/Cursor.2.4.png",
-        "Cursor/Cursor.2.5.png",
-        "Cursor/Cursor.2.6.png",
+        {
+            path: "Cursor.3.0.png",
+            size: new Size(34, 31),
+            offset: new Point(14, -22),
+            delay: 200,
+        },
+        {
+            path: "Cursor.3.1.png",
+            size: new Size(39, 37),
+            offset: new Point(15.5, -22),
+            delay: 200,
+        },
+        {
+            path: "Cursor.3.2.png",
+            size: new Size(38, 37),
+            offset: new Point(16, -22),
+            delay: 200,
+        },
+        {
+            path: "Cursor.3.3.png",
+            size: new Size(35, 31),
+            offset: new Point(13.5, -22),
+            delay: 200,
+        },
+        {
+            path: "Cursor.3.4.png",
+            size: new Size(38, 37),
+            offset: new Point(16, -22),
+            delay: 200,
+        },
+        {
+            path: "Cursor.3.5.png",
+            size: new Size(39, 37),
+            offset: new Point(15.5, -22),
+            delay: 200,
+        },
     ],
     [
-        "Cursor/Cursor.3.0.png",
-        "Cursor/Cursor.3.1.png",
-        "Cursor/Cursor.3.2.png",
-        "Cursor/Cursor.3.3.png",
-        "Cursor/Cursor.3.4.png",
-        "Cursor/Cursor.3.5.png",
+        {
+            path: "Cursor.4.0.png",
+            size: new Size(29, 30),
+            offset: new Point(11.5, -24),
+            delay: 500,
+        },
+        {
+            path: "Cursor.4.1.png",
+            size: new Size(30, 28),
+            offset: new Point(11, -24),
+            delay: 500,
+        },
     ],
     [
-        "Cursor/Cursor.4.0.png",
-        "Cursor/Cursor.4.1.png",
+        {
+            path: "Cursor.5.0.png",
+            size: new Size(27, 29),
+            offset: new Point(7.5, -22),
+            delay: 250,
+        },
+        {
+            path: "Cursor.5.1.png",
+            size: new Size(25, 23),
+            offset: new Point(6.5, -20),
+            delay: 100,
+        },
+        {
+            path: "Cursor.5.2.png",
+            size: new Size(22, 21),
+            offset: new Point(6, -19),
+            delay: 200,
+        },
     ],
     [
-        "Cursor/Cursor.5.0.png",
-        "Cursor/Cursor.5.1.png",
-        "Cursor/Cursor.5.2.png",
+        {
+            path: "Cursor.6.0.png",
+            size: new Size(32, 28),
+            offset: new Point(13, -22),
+            delay: 200,
+        },
+        {
+            path: "Cursor.6.1.png",
+            size: new Size(33, 26),
+            offset: new Point(12.5, -22),
+            delay: 100,
+        },
+        {
+            path: "Cursor.6.2.png",
+            size: new Size(33, 26),
+            offset: new Point(12.5, -22),
+            delay: 100,
+        },
+        {
+            path: "Cursor.6.3.png",
+            size: new Size(32, 28),
+            offset: new Point(13, -22),
+            delay: 100,
+        },
+        {
+            path: "Cursor.6.4.png",
+            size: new Size(32, 28),
+            offset: new Point(13, -22),
+            delay: 100,
+        },
+        {
+            path: "Cursor.6.5.png",
+            size: new Size(33, 26),
+            offset: new Point(12.5, -22),
+            delay: 100,
+        },
+        {
+            path: "Cursor.6.6.png",
+            size: new Size(33, 26),
+            offset: new Point(12.5, -22),
+            delay: 100,
+        },
     ],
     [
-        "Cursor/Cursor.6.0.png",
-        "Cursor/Cursor.6.1.png",
-        "Cursor/Cursor.6.2.png",
-        "Cursor/Cursor.6.3.png",
-        "Cursor/Cursor.6.4.png",
-        "Cursor/Cursor.6.5.png",
-        "Cursor/Cursor.6.6.png",
+        {
+            path: "Cursor.7.0.png",
+            size: new Size(33, 33),
+            offset: new Point(4.5, -22),
+            delay: 200,
+        },
+        {
+            path: "Cursor.7.1.png",
+            size: new Size(33, 35),
+            offset: new Point(4.5, -22),
+            delay: 200,
+        },
+        {
+            path: "Cursor.7.2.png",
+            size: new Size(33, 36),
+            offset: new Point(4.5, -22),
+            delay: 200,
+        },
+        {
+            path: "Cursor.7.4.png",
+            size: new Size(33, 35),
+            offset: new Point(4.5, -22),
+            delay: 200,
+        },
     ],
     [
-        "Cursor/Cursor.7.0.png",
-        "Cursor/Cursor.7.1.png",
-        "Cursor/Cursor.7.2.png",
-        "Cursor/Cursor.7.4.png",
+        {
+            path: "Cursor.8.0.png",
+            size: new Size(30, 35),
+            offset: new Point(6, -22),
+            delay: 200,
+        },
+        {
+            path: "Cursor.8.1.png",
+            size: new Size(32, 35),
+            offset: new Point(5, -22),
+            delay: 200,
+        },
+        {
+            path: "Cursor.8.2.png",
+            size: new Size(33, 35),
+            offset: new Point(4.5, -22),
+            delay: 200,
+        },
+        {
+            path: "Cursor.8.4.png",
+            size: new Size(32, 35),
+            offset: new Point(5, -22),
+            delay: 200,
+        },
     ],
     [
-        "Cursor/Cursor.8.0.png",
-        "Cursor/Cursor.8.1.png",
-        "Cursor/Cursor.8.2.png",
-        "Cursor/Cursor.8.4.png",
+        {
+            path: "Cursor.9.0.png",
+            size: new Size(33, 36),
+            offset: new Point(4.5, -22),
+        },
     ],
-    ["Cursor/Cursor.9.0.png"],
-    ["Cursor/Cursor.10.0.png"],
-    ["Cursor/Cursor.11.0.png"],
-    ["Cursor/Cursor.12.0.png"],
     [
-        "Cursor/Cursor.13.0.png",
-        "Cursor/Cursor.13.1.png",
+        {
+            path: "Cursor.10.0.png",
+            size: new Size(33, 35),
+            offset: new Point(4.5, -22),
+        },
+    ],
+    [
+        {
+            path: "Cursor.11.0.png",
+            size: new Size(22, 21),
+            offset: new Point(6, -19),
+        },
+    ],
+    [
+        {
+            path: "Cursor.12.0.png",
+            size: new Size(25, 23),
+            offset: new Point(8.5, -22),
+        },
+    ],
+    [
+        {
+            path: "Cursor.13.0.png",
+            size: new Size(29, 30),
+            offset: new Point(11.5, -24),
+            delay: 500,
+        },
+        {
+            path: "Cursor.13.1.png",
+            size: new Size(30, 28),
+            offset: new Point(11, -24),
+            delay: 500,
+        },
     ],
 ]
-
-export class Cursor {
-    position: [number, number] = [0, 0];
-    state: CursorState;
-};
