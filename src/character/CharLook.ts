1969ac15336501befe922b7dae59eb05116852f8
LuisHsu <luishsuplus@gmail.com>
2022-10-20 01:10:25 -0700

    Update charlook climbing

--- src/character/CharLook.ts
+++ src/character/CharLook.ts
@@ -112,7 +112,7 @@ export class CharLook implements Drawable{
         }
     }
 
-    private make_animations(stance_id: Stance.Id = Stance.Id.shot){
+    private make_animations(stance_id: Stance.Id = Stance.Id.stand1){
         const body = this.body.stances[stance_id];
         const hair = this.hair.stances[stance_id];
         // Merge stances
@@ -139,78 +139,103 @@ export class CharLook implements Drawable{
         }
 
         // Generate animations
-        this.animation.lower = new Animation(Object.values(stances).map(frame => {
-            let item: FrameItem[] = [];
-            const {body, hair} = frame.layers;
-            // TODO: climbing
-            if(hair[Hair.Layer.belowbody]){
-                item.push(hair[Hair.Layer.belowbody]);
-            }
-            
-            if(body[Body.Layer.body]){
-                item.push(body[Body.Layer.body]);
-            }else if(body[Body.Layer.back_body]){
-                item.push(body[Body.Layer.back_body]);
-            }
-            if(body[Body.Layer.arm_below_head]){
-                item.push(body[Body.Layer.arm_below_head]);
-            }
-            if(body[Body.Layer.arm_below_head_over_mail]){
-                item.push(body[Body.Layer.arm_below_head_over_mail]);
-            }
-            if(body[Body.Layer.head]){
-                item.push(body[Body.Layer.head]);
-            }
+        if(Stance.is_climbing(stance_id)){
+            this.animation.lower = null;
+            this.animation.upper = new Animation(Object.values(stances).map(frame => {
+                let item: FrameItem[] = [];
+                const {body, hair} = frame.layers;
 
-            if(hair[Hair.Layer.shade]){
-                item.push(hair[Hair.Layer.shade]);
-            }
-            if(hair[Hair.Layer.default]){
-                item.push(hair[Hair.Layer.default]);
-            }
-            return new Frame(item, frame.delay, null, null, null, () => {
-                if(frame.has_face){
-                    this.face_pos = frame.positions.face;
-                }else{
-                    this.face_pos = null;
+                if(body[Body.Layer.back_body]){
+                    item.push(body[Body.Layer.back_body]);
                 }
-            });
-        }));
+                if(body[Body.Layer.head]){
+                    item.push(body[Body.Layer.head]);
+                }
+                if(hair[Hair.Layer.back]){
+                    item.push(hair[Hair.Layer.back]);
+                }
+                // TODO: [equip] weapon over hand
+                // TODO: [equip] weapon over body
+                // if(body[Body.Layer.hand_over_weapon]){
+                //     textures.push(body[Body.Layer.hand_over_weapon]);
+                // }
+                // TODO: [equip] Wrist over hair
+                // TODO: [equip] Glove over hair
+                return new Frame(item, frame.delay);
+            }));
+        }else{
+            this.animation.lower = new Animation(Object.values(stances).map(frame => {
+                let item: FrameItem[] = [];
+                const {body, hair} = frame.layers;
+                if(hair[Hair.Layer.belowbody]){
+                    item.push(hair[Hair.Layer.belowbody]);
+                }
+                if(body[Body.Layer.body]){
+                    item.push(body[Body.Layer.body]);
+                }
+                if(body[Body.Layer.arm_below_head]){
+                    item.push(body[Body.Layer.arm_below_head]);
+                }
+                if(body[Body.Layer.arm_below_head_over_mail]){
+                    item.push(body[Body.Layer.arm_below_head_over_mail]);
+                }
+                if(body[Body.Layer.head]){
+                    item.push(body[Body.Layer.head]);
+                }
 
-        this.animation.upper = new Animation(Object.values(stances).map(frame => {
-            let item: FrameItem[] = [];
-            const {body, hair} = frame.layers;
-
-            if(body[Body.Layer.arm]){
-                item.push(body[Body.Layer.arm]);
-            }
-            // TODO: [equip] wrist
-            // TODO: [equip] glove
-            // TODO: [equip] weapon over glove
-            if(body[Body.Layer.hand_below_weapon]){
-                item.push(body[Body.Layer.hand_below_weapon]);
-            }
-            if(body[Body.Layer.arm_over_hair]){
-                item.push(body[Body.Layer.arm_over_hair]);
-            }
-            if(body[Body.Layer.arm_over_hair_below_weapon]){
-                item.push(body[Body.Layer.arm_over_hair_below_weapon]);
-            }
-            // TODO: [equip] weapon over hand
-            // TODO: [equip] weapon over body
-            if(body[Body.Layer.hand_over_hair]){
-                item.push(body[Body.Layer.hand_over_hair]);
-            }
-            // if(body[Body.Layer.hand_over_weapon]){
-            //     textures.push(body[Body.Layer.hand_over_weapon]);
-            // }
-            // TODO: [equip] Wrist over hair
-            // TODO: [equip] Glove over hair
-            return new Frame(item, frame.delay);
-        }));
+                if(hair[Hair.Layer.shade]){
+                    item.push(hair[Hair.Layer.shade]);
+                }
+                if(hair[Hair.Layer.default]){
+                    item.push(hair[Hair.Layer.default]);
+                }
+                return new Frame(item, frame.delay, null, null, null, () => {
+                    if(frame.has_face){
+                        this.face_pos = frame.positions.face;
+                    }else{
+                        this.face_pos = null;
+                    }
+                });
+            }));
+            this.animation.upper = new Animation(Object.values(stances).map(frame => {
+                let item: FrameItem[] = [];
+                const {body, hair} = frame.layers;
+                // TODO: [equip] Eyeacc
+                // TODO: [equip] Shield
+                // TODO: [equip] captype
+                // TODO: [equip] Weapon
+                if(body[Body.Layer.arm]){
+                    item.push(body[Body.Layer.arm]);
+                }
+                // TODO: [equip] wrist
+                // TODO: [equip] glove
+                // TODO: [equip] weapon over glove
+                if(body[Body.Layer.hand_below_weapon]){
+                    item.push(body[Body.Layer.hand_below_weapon]);
+                }
+                if(body[Body.Layer.arm_over_hair]){
+                    item.push(body[Body.Layer.arm_over_hair]);
+                }
+                if(body[Body.Layer.arm_over_hair_below_weapon]){
+                    item.push(body[Body.Layer.arm_over_hair_below_weapon]);
+                }
+                // TODO: [equip] weapon over hand
+                // TODO: [equip] weapon over body
+                if(body[Body.Layer.hand_over_hair]){
+                    item.push(body[Body.Layer.hand_over_hair]);
+                }
+                if(body[Body.Layer.hand_over_weapon]){
+                    item.push(body[Body.Layer.hand_over_weapon]);
+                }
+                // TODO: [equip] Wrist over hair
+                // TODO: [equip] Glove over hair
+                return new Frame(item, frame.delay);
+            }));
+        }
     }
 
     private make_face(expression: Expression = Expression.default){
         this.animation.face = this.face.expressions[expression];
+        // TODO: [equip] Face
     }
 }
